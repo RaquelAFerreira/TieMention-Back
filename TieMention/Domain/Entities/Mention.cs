812697197fdb799a4e0e9748c1d4ae5eeebe7310
RaquelAFerreira <raquelalmferreira@gmail.com>
2025-05-08 14:59:42 -0300

    .

--- TieMention/Domain/Entities/Mention.cs
+++ TieMention/Domain/Entities/Mention.cs
@@ -4,4 +4,15 @@ public class Mention
 {
     public Guid Id { get; set; }
     public string Description { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public Guid MentioningPiece { get; set; }
+
+    public Guid MentionedPiece { get; set; }
+
+    public bool IsApproved { get; set; } = false;
+
 }
