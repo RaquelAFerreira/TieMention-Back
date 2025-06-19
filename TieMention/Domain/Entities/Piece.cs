49acf7f01bc8dbecbc36cd7b98b72ddbf870b346
RaquelAFerreira <raquelalmferreira@gmail.com>
2025-06-19 14:37:48 -0300

    .

--- TieMention/Domain/Entities/Piece.cs
+++ TieMention/Domain/Entities/Piece.cs
@@ -1,18 +1,12 @@
 namespace TieMention.Domain.Entities;
 
-public class Piece
+public class Piece : BaseEntity
 {
-    public Guid Id { get; set; }
-
     public string Name { get; set; } = string.Empty;
 
     public string Description { get; set; } = string.Empty;
 
     public Guid Category { get; set; }
-
-    public DateTime CreatedAt { get; set; }
-
-    public DateTime UpdatedAt { get; set; }
 
     public string ReleaseYear { get; set; } = string.Empty;
 
